c0607888939f2b0f2a87c5f25666f7b261a90fd4
sckimynwa <tsb03672@naver.com>
2023-07-30 21:52:03 +0900

    feat: add styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "@neo4j-ndl/base/lib/neo4j-ds-styles.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,35 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  presets: [require("@neo4j-ndl/base").tailwindConfig],
+  prefix: "",
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  // The preset has this safelist, but with the "n-" prefix. Should be removed eventually as per
+  safelist: [
+    {
+      pattern:
+        /^(hover:)?text-(light|primary|danger|warning|success|blueberry|mint|neutral)-/,
+      variants: ["hover"],
+    },
+    {
+      pattern:
+        /^(hover:)?bg-(light|primary|danger|warning|success|blueberry|mint|neutral)-/,
+      variants: ["hover"],
+    },
+    {
+      // For clicked classes
+      pattern: /^(active:)?bg-light-/,
+      variants: ["active"],
+    },
+    {
+      pattern: /^(hover:)?border-light-/,
+      variants: ["hover"],
+    },
+  ],
+  plugins: [require("daisyui"), require('@tailwindcss/typography')],
+};
 
